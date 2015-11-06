185fede64632daf0877eb8e332c21ddf8c439fd5
Jason Dentler <jasondentler@gmail.com>
2015-11-06 09:01:40 -0600

    New VS Extension: Bugger It!

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # ![Lightning Bug](http://lightningbug.jasondentler.com/images/lightningbug_vector.png) [Lightning Bug](http://lightningbug.jasondentler.com)
 
-[Lightning Bug](http://lightningbug.jasondentler.com) is a collection of flashy but short-lived bits of code for common tasks. 
+[Lightning Bug](http://lightningbug.jasondentler.com) is a collection of flashy but short-lived bits of code for common tasks, visual studio extensions, and more.
 
 Visit [http://lightningbug.jasondentler.com](http://lightningbug.jasondentler.com) for more info.
---
+++ src/LightningBug.VisualStudioExtensions.Bugger/BuggerItCommand.cs
@@ -0,0 +1,101 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="BuggerItCommand.cs" company="Microsoft">
+//     Copyright (c) Microsoft.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace LightningBug.VisualStudioExtensions.Bugger
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class BuggerItCommand
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("1a2c3f53-7f9a-433b-b394-dc166b486668");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly Package _package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BuggerItCommand"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        private BuggerItCommand(Package package)
+        {
+            if (package == null)
+                throw new ArgumentNullException(nameof(package));
+
+            _package = package;
+
+            var commandService = ServiceProvider.GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+
+            if (commandService == null) return;
+
+            var menuCommandId = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(MenuItemCallback, menuCommandId);
+            commandService.AddCommand(menuItem);
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static BuggerItCommand Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private IServiceProvider ServiceProvider => _package;
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static void Initialize(Package package)
+        {
+            Instance = new BuggerItCommand(package);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void MenuItemCallback(object sender, EventArgs e)
+        {
+            var message = string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", GetType().FullName);
+            const string title = nameof(BuggerItCommand);
+
+            // Show a message box to prove we were here
+            VsShellUtilities.ShowMessageBox(
+                ServiceProvider,
+                message,
+                title,
+                OLEMSGICON.OLEMSGICON_INFO,
+                OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+        }
+    }
+}
