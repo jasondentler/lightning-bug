8d805a33adf45e6214e4f958d80f0267b9ecf896
Jason Dentler <jasondentler@gmail.com>
2015-11-19 14:39:31 -0600

    Add command operations

---
+++ src/LightningBug/Data/ETL/Operations/SqlCommandOperation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace LightningBug.Data.ETL.Operations
+{
+    public abstract class SqlCommandOperation<T> : CommandOperation<T>
+    {
+        private readonly Func<SqlConnection> _connectionProvider;
+
+        protected SqlCommandOperation(Func<SqlConnection> connectionProvider)
+        {
+            _connectionProvider = connectionProvider;
+        }
+
+        public override void OnInputReady()
+        {
+            base.OnInputReady();
+            using (var conn = _connectionProvider())
+            {
+                conn.Open();
+                ExecuteSql(conn);
+            }
+        }
+
+        protected abstract void ExecuteSql(SqlConnection connection);
+
+    }
+}
